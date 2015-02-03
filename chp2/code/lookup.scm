cd5489b68d68fdab8bc2460351ef359eb90049c7
huangjing <hj005zy@gmail.com>
2015-02-04 06:41:57 +0800

    add code of chp2

---
+++ chp2/code/lookup.scm
@@ -0,0 +1,7 @@
+;;; lookup.scm
+
+(define (lookup given-key set-of-records)
+    (cond ((null? set-of-records) false)
+          ((equal? given-key (key (car set-of-records)))
+           (car set-of-records))
+          (else (lookup given-key (cdr set-of-records)))))
